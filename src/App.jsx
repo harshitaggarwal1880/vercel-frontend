86bd4400947e5db0145121fd384bc7ba92e3b87e
harshit <harshit@zeddlabz.com>
2023-09-11 17:03:31 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 const App = () => {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[5vw] font-bold">App</div>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[5vw] font-bold">{import.meta.env.VITE_VARIABLE}</div>
   )
 }
 
